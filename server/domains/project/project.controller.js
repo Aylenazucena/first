d55383eb32ff048bfd6b3847f0de7db6ff77ade2
Aylenazucena <142960710+Aylenazucena@users.noreply.github.com>
2023-11-17 03:43:46 +0000

    S04A01 Validación con Yup

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,29 +1,39 @@
+import log from '../../config/winston';
+
 // Actions methods
-// GET "/project"
+
+// GET '/project/showDashboard'
 const showDashboard = (req, res) => {
-  res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+  res.send("🚧 Under Construction '/project/showDashboard' 🚧");
 };
-// GET "/project/add"
-const add = (req, res) => {
+// GET '/project/addForm'
+const addForm = (req, res) => {
   res.render('project/addView');
 };
 
 // POST "/project/add"
 const addPost = (req, res) => {
-  // Extrayendo la informacion
-  // del formulario
-  const { name, description } = req.body;
-  // Regresando al cliente la información recabada
-  res.status(200).json({
-    name,
-    description,
-  });
+  // Rescatando la info del formulario
+  const { errorData: validationError } = req;
+  // En caso de haber error
+  // se le informa al cliente
+  if (validationError) {
+    log.info('Se entrega al cliente error de validación de add Project');
+    res.status(422).json(validationError);
+  } else {
+    // En caso de que pase la validación
+    // Se desestructura la información
+    // de la peticion
+    const { validData: project } = req;
+    // Se contesta la información
+    // del proyecto al cliente
+    res.status(200).json(project);
+  }
 };
-
 // Controlador user
 export default {
   // Action Methods
   showDashboard,
-  add,
+  addForm,
   addPost,
 };
--- server/domains/project/project.router.js
+++ server/domains/project/project.router.js
@@ -1,17 +1,33 @@
 // Importando el Router de Express
 import { Router } from 'express';
+
 // Importando el controlador
 import projectController from './project.controller';
+
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import projectValidator from './project.validator';
+
 // Creando una isntancia del enrutador
 const router = new Router();
-// Enrutamos
-// GET "/project"
-router.get('/', projectController.showDashboard);
-// GET "/project/add"
-router.get('/add', projectController.add);
 
+// Enrutamos
+// GET '/project/'
+// GET '/project/dashboard'
+router.get(['/', '/dashboard'], projectController.showDashboard);
+// GET '/project/add-form'
+// GET '/project/add'
+router.get(['/add-form', '/add'], projectController.addForm);
 // POST "/project/add"
-router.post('/add', projectController.addPost);
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addPost,
+);
 
 // Exporto este tramo de ruta
 export default router;
