664a93b039498ee073138b37542660d4874af1e6
Aylenazucena <142960710+Aylenazucena@users.noreply.github.com>
2023-11-19 05:29:23 +0000

    S04A03 Persistencia de datos

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,4 +1,6 @@
 import log from '../../config/winston';
+// Importando el modelo
+import ProjectModel from './project.model';
 
 // Actions methods
 
@@ -12,7 +14,7 @@ const addForm = (req, res) => {
 };
 
 // POST "/project/add"
-const addPost = (req, res) => {
+const addPost = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
@@ -35,10 +37,18 @@ const addPost = (req, res) => {
   // Se desestructura la información
   // de la peticion
   const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del proyecto cargado');
-  return res.status(200).json(project);
+  try {
+    // Creando la instancia de un documento con los valores de 'project'
+    const savedProject = await ProjectModel.create(project);
+    // Se contesta la información del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    return res.status(200).json(savedProject);
+  } catch (error) {
+    log.error(
+      'ln 53 project.controller: Error al guardar proyecto en la base de datos',
+    );
+    return res.status(500).json(error);
+  }
 };
 // Controlador user
 export default {
