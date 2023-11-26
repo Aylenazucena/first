cff04572b4b825d751740ea79e78685aaaf79f8b
Aylenazucena <142960710+Aylenazucena@users.noreply.github.com>
2023-11-26 01:53:34 +0000

    S04A07 Registro y login

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,12 +1,15 @@
 // Actions methods
 // GET "/"
 // GET "/index"
+import log from '../../config/winston';
+// Action Methods
 
 // GET '/user/login'
 const login = (req, res) => {
-  res.send("👷‍♀️ UNDER CONSTRUCTION GET '/user/login' 👷‍♀️");
+  // Sirve el formulario de login
+  log.info('Se entrega formulario de login');
+  res.render('user/login');
 };
-
 // GET '/user/logout'
 const logout = (req, res) => {
   res.send("👷‍♀️ UNDER CONSTRUCTION GET '/user/logout' 👷‍♀️");
@@ -14,7 +17,8 @@ const logout = (req, res) => {
 
 // GET '/user/register'
 const register = (req, res) => {
-  res.send("👷‍♀️ UNDER CONSTRUCTION GET '/user/register' 👷‍♀️");
+  log.info('Se entrega formulario de registro');
+  res.render('user/register');
 };
 // Controlador Home
 export default {
